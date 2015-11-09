4ddb9692cbb3392343d8f40073ee122af9bb8c1a
Antoine Vandecreme <ant.vand@gmail.com>
2015-11-08 19:10:08 -0500

    Revert chromium to use URL parameter to avoid breaking history.

--- chromium/chromium.js
+++ chromium/chromium.js
@@ -34,16 +34,7 @@
         title: "View with OpenSeadragon",
         onclick: function (event) {
             chrome.tabs.update({
-                url: "visualization.html"
-            }, function (tab) {
-                function messageHandler(message, sender, sendResponse) {
-                    if (sender.tab.id === tab.id &&
-                            message.request === "getUrl") {
-                        chrome.runtime.onMessage.removeListener(messageHandler);
-                        sendResponse({url: event.srcUrl});
-                    }
-                }
-                chrome.runtime.onMessage.addListener(messageHandler);
+                url: "visualization.html?img=" + event.srcUrl
             });
         }
     });
